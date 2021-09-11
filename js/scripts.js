7c57de09617d319304c0357a4dac517f81dcb8c2
Sudeep Bishwakaarma <sudeepbishwakarma2@gmail.com>
2021-09-11 12:14:33 -0400

    Week 4

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+
+$(function() {
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal('show');
+    });
+
+    $("#loginButton").click(function(){
+        $("#loginModal").modal('show');
+    } );
+    
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause"); 
+        }
+    });
+});
